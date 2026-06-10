6b7e4eb8889fe4e134926e24dbc889f80b80c8d4
hyomin-dev <choihyomin248@gmail.com>
2026-06-11 07:14:33 +0900

    add controller, service code

--- src/main/java/me/hyomin/sonartest/BasicController.java
+++ src/main/java/me/hyomin/sonartest/BasicController.java
@@ -17,4 +17,42 @@ public class BasicController {
         return basicService.basic();
     }
 
+    @GetMapping("/2")
+    public String basic2(){
+        return basicService.basic2();
+    }
+    @GetMapping("/3")
+    public String basic3(){
+        return basicService.basic3();
+    }
+    @GetMapping("/4")
+    public String basic4(){
+        return basicService.basic4();
+    }
+    @GetMapping("/")
+    public String basic5(){
+        return basicService.basic5();
+    }
+    @GetMapping("/")
+    public String basic6(){
+        return basicService.basic6();
+    }
+    @GetMapping("/7")
+    public String basic7(){
+        return basicService.basic7();
+    }
+    @GetMapping("/8")
+    public String basic8(){
+        return basicService.basic8();
+    }
+    @GetMapping("/9")
+    public String basic9(){
+        return basicService.basic9();
+    }
+    @GetMapping("/10")
+    public String basic10(){
+        return basicService.basic10();
+    }
+
+
 }
--- src/main/java/me/hyomin/sonartest/BasicService.java
+++ src/main/java/me/hyomin/sonartest/BasicService.java
@@ -8,4 +8,41 @@ public class BasicService {
     public String basic(){
         return "Hello World";
     }
+
+    public String basic2(){
+        return "Hello World2";
+    }
+
+    public String basic3(){
+        return "Hello World3";
+    }
+
+    public String basic4(){
+        return "Hello World4";
+    }
+
+    public String basic5(){
+        return "Hello World5";
+    }
+
+    public String basic6(){
+        return "Hello World6";
+    }
+
+    public String basic7(){
+        return "Hello World7";
+    }
+
+    public String basic8(){
+        return "Hello World8";
+    }
+
+    public String basic9(){
+        return "Hello World9";
+    }
+
+    public String basic10(){
+        return "Hello World10";
+    }
+
 }
